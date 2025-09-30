981b2bb3b3cfc21b4c5031f4e19896a6f88e75bc
JaapSikkenk <jaap.sikkenk@i-sociaaldomein.nl>
2025-09-30 16:28:00 +0200

    Update prompt.ts

--- packages/api/src/prompt.ts
+++ packages/api/src/prompt.ts
@@ -514,6 +514,7 @@ All tables must be formatted in proper markdown with vertical bars and dashes wh
   "Begrippenlijst_iJw_en_iWmo":   "https://i-sociaal-lab.github.io/jaapjunior/Begrippenlijst-Jw-en-Wmo.html",
   "Casusbeschrijvingen": "https://www.istandaarden.nl/binaries/content/assets/istandaarden/iwmo/iwmo-3.2/casusbeschrijvingen-iwmo-3.2-en-ijw-3.2.pdf",
   "Codelijsten iJw release 3.2":  "https://informatiemodel.istandaarden.nl/informatiemodel/ijw/3.2/codelijsten/",
+  "[CODENR]_[CONCEPT]": "https://informatiemodel.istandaarden.nl/informatiemodel/ijw/3.2/codelijsten/",
   "Gemeentecodes_CBS":   "https://www.cbs.nl/nl-nl/onze-diensten/methoden/classificaties/overig/gemeentelijke-indelingen-per-jaar/indeling-per-jaar/gemeentelijke-indeling-op-1-januari-2025",
   "Jeugdwet":            "https://wetten.overheid.nl/BWBR0034925/2025-01-01",
   "Basisschema.xsd":      "https://www.istandaarden.nl/ijw/releases/release-ijw-3.2",
